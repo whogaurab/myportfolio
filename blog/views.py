822c26190d87ad137a643a6ef51821867c3caa13
whogaurab <gaurabchoudhary2002@gmail.com>
2024-07-29 00:34:57 +0530

    blog site working properly

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 # blog/views.py
 
-
+from django.http import HttpResponseRedirect
 from blog.models import Post, Comment
+from blog.forms import CommentForm
+
 
 def blog_index(request):
     posts = Post.objects.all().order_by("-created_on")
@@ -30,10 +32,22 @@ def blog_category(request, category):
 
 def blog_detail(request, pk):
     post = Post.objects.get(pk=pk)
+    form = CommentForm()
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = Comment(
+                author=form.cleaned_data["author"],
+                body=form.cleaned_data["body"],
+                post=post,
+            )
+            comment.save()
+            return HttpResponseRedirect(request.path_info)
     comments = Comment.objects.filter(post=post)
     context = {
         "post": post,
         "comments": comments,
+        "form": CommentForm(),
     }
 
     return render(request, "blog/detail.html", context)
